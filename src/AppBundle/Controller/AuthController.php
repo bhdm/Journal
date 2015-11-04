b1b7ee12ffb90ad5eda7faf107d2712754c24aa5
bhd.m@ya.ru <bhd.m@ya.ru>
2015-11-04 13:43:43 +0300

    updates

--- src/AppBundle/Controller/AuthController.php
+++ src/AppBundle/Controller/AuthController.php
@@ -2,10 +2,13 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\ProfileType;
+use AppBundle\Form\UserType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class AuthController extends Controller
 {
@@ -14,6 +17,34 @@ class AuthController extends Controller
      * @Template()
      */
     public function loginAction(){
-        return [];
+
+        if ($this->get('request')->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $this->get('request')->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $this->get('request')->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
+        }
+        return array(
+            'error' => $error
+        );
+    }
+
+    /**
+     * @Route("/profile", name="profile")
+     * @Template()
+     */
+    public function profileAction(Request $request){
+        $em = $this->getDoctrine()->getManager();
+        $item = $em->getRepository('AppBundle:User')->findOneById($this->getUser());
+        $form = $this->createForm(new ProfileType($em), $item);
+        $formData = $form->handleRequest($request);
+        if ($formData->isValid()){
+            $item = $formData->getData();
+            $em->persist($item);
+            $em->flush();
+            $em->refresh($item);
+            return $this->redirect($this->generateUrl('journal_list'));
+        }
+
+        return ['form' => $form->createView(), 'journal' => $item];
     }
 }
