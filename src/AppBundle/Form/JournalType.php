1a30eb224bc3fba48552f4b0a9870904e4a780ae
bhd.m@ya.ru <bhd.m@ya.ru>
2015-11-04 17:29:15 +0300

    u

--- src/AppBundle/Form/JournalType.php
+++ src/AppBundle/Form/JournalType.php
@@ -16,7 +16,7 @@ class JournalType extends AbstractType
     {
         $builder
             ->add('title', null, ['label' => 'Название'])
-            ->add('photo', 'file', ['label' => 'Фото обложки'])
+            ->add('photo', 'file', ['label' => 'Фото обложки', 'data_class' => null])
             ->add('year', null, ['label' => 'Год выпуска'])
             ->add('month', null, ['label' => 'Месяц выпуска'])
             ->add('enabled','choice',  array(
